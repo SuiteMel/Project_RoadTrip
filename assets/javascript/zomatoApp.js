c5e27174aeb1b13ed54b9a8bdb2dca559c75051d
Melissa Waltner <melissamwaltner@gmail.com>
2018-03-11 14:45:59 -0500

    Added file links to index

--- assets/javascript/zomatoApp.js
+++ assets/javascript/zomatoApp.js
@@ -1,12 +1,17 @@
-// var queryURL = "https://developers.zomato.com/api/v2.1/geocode?lat=38.8989439&lon=-94.7256576";
+
+
+var userInput = "";
 
 var latitude = "38.8989439";
 var longitude = "-94.7256576";
 
-var queryURL = "https://developers.zomato.com/api/v2.1/geocode";
+var queryURL = "https://developers.zomato.com/api/v2.1/search";
   queryURL += '?' + $.param({
     'lat': latitude,
-    'lon': longitude
+    'lon': longitude,
+    'sort': "real_distance",
+    'start': 0,
+    'q': userInput
   });
 
   $.ajax({
@@ -14,15 +19,16 @@ var queryURL = "https://developers.zomato.com/api/v2.1/geocode";
     headers: {"user-key": "347df1f1ac7c392cc9a8c55d2bbf3ed3"},
     method: 'GET'
   }).then(function (response) {
-    var resName = response.nearby_restaurants[0].restaurant.name;
-    var resLocation = response.nearby_restaurants[0].restaurant.location.address;
+    var resName = response.restaurants[0].restaurant.name;
+    var resLocation = response.restaurants[0].restaurant.location.address;
 
-    var resCuisine = response.nearby_restaurants[0].restaurant.cuisines;
-    var price_range = response.nearby_restaurants[0].restaurant.price_range;
-    var currency = response.nearby_restaurants[0].restaurant.currency;
+    var resCuisine = response.restaurants[0].restaurant.cuisines;
+    var price_range = response.restaurants[0].restaurant.price_range;
+    var currency = response.restaurants[0].restaurant.currency;
 
-    var resRating = response.nearby_restaurants[0].restaurant.user_rating.aggregate_rating;
-    var ratingText = response.nearby_restaurants[0].restaurant.user_rating.rating_text;
-    var resVotes = response.nearby_restaurants[0].restaurant.user_rating.votes;
+    var resRating = response.restaurants[0].restaurant.user_rating.aggregate_rating;
+    var ratingText = response.restaurants[0].restaurant.user_rating.rating_text;
+    var resVotes = response.restaurants[0].restaurant.user_rating.votes;
 
+    console.log(response.results_found);
   });
