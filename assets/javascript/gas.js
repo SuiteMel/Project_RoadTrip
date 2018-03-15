8786922f1621438c489350a27fd5302438fd8dbd
Trisha McCoy <trishamccoy@Trishas-MacBook-Pro.local>
2018-03-14 19:45:45 -0500

    updated gas.js

--- assets/javascript/gas.js
+++ assets/javascript/gas.js
@@ -1,10 +1,13 @@
- $(document).ready(function () {
+$(document).ready(function () {
+
+var lati=38;
+var longitude=-94;
     
     $("#gasStation").on("click", function(event) {
         event.preventDefault();
 
         var station = $(this).data('name');
-        var apiUrl = "http://api.mygasfeed.com/stations/radius/38/-94/25/reg/distance/dfoh89ze54.json";
+        var apiUrl = "http://api.mygasfeed.com/stations/radius/"+ lati +"/" + longitude +"/10/reg/distance/dfoh89ze54.json";
         $.ajax({
             url: apiUrl,
             method: 'GET'
@@ -17,28 +20,42 @@
  }); // end of onDocumentReady
 
 var displayGasList = function(gasList) {
+    $("#gasStation").empty();//clear button
 
-    // clear box
+  $("#gas").empty();  // clear box
 
-    // create list
+   // create list
      for (var i = 0; i < 5; i++) {
         var gasLatitude=gasList[i].lat;
         console.log(gasLatitude)
         var gasLongitude = gasList[i].lng;
         console.log(gasLongitude)
-        var gasName = gasList[i].station;
+        var gasName =$("<dt>").text(gasList[i].station);
         console.log(gasName)
-        var gasDistance = gasList[i].distance;
+        var gasDistance =$("<dd>").text(gasList[i].distance).addClass("mb-0");
         console.log(gasDistance)
-        var gasAddress = gasList[i].address;
+
+        var gasZip = gasList[i].zip;
+        console.log(gasZip)
+        var gasAddress = $("<dd>").text(gasList[i].address).addClass("mb-0");
         console.log(gasAddress)
         var gasCity = gasList[i].city;
         console.log(gasCity)
-        var gasPrice = gasList[i].reg_price;
+
+        var gasLoc= $("<dd>").text(gasCity + " " + gasZip).addClass("mb-0");
+
+
+        var gasPrice = $("<dd>").text("Regular: "+gasList[i].reg_price).addClass("mb-0");
         console.log(gasPrice)
-        var dieselPrice = gasList[i].diesel_price;
+        var dieselPrice = $("<dd>").text("Diesel: "+gasList[i].diesel_price).addClass("mb-0");
         console.log(dieselPrice)
+
+
+        var gasDesc = $("<dl>").append(gasName, gasDistance,gasAddress, gasLoc, gasPrice, dieselPrice);
+    $("#gas").append(gasDesc);
+
+        
     }
 
-    // add to box
+    
 };
